7c56c0e1daa987b512e0a78d4440cbd28ff3c8bd
sachhm <sachhmoka@gmail.com>
2024-07-10 18:10:21 +1000

    Initial Commit

---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Sachh Moka on 10/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
